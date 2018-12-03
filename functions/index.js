c64f9a946b3c873fe966c0d2d8bf377821473c1c
Filipe Merker <filipesmerker@gmail.com>
2018-12-03 16:30:00 -0200

    Create boilerplate for firestore methods

--- functions/index.js
+++ functions/index.js
@@ -17,70 +17,60 @@
 
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const firebaseHelper = require('firebase-functions-helper')
 admin.initializeApp()
 const express = require('express')
 const cookieParser = require('cookie-parser')()
+const bodyParser = require('body-parser')
 const cors = require('cors')({ origin: true })
+const db = admin.firestore()
 const app = express()
+const tasksCollection = 'tasks'
 
-// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
-// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
-// `Authorization: Bearer <Firebase ID Token>`.
-// when decoded successfully, the ID Token content will be added as `req.user`.
-const validateFirebaseIdToken = async (req, res, next) => {
-  console.log('Check if request is authorized with Firebase ID token')
-
-  if (
-    (!req.headers.authorization ||
-      !req.headers.authorization.startsWith('Bearer ')) &&
-    !(req.cookies && req.cookies.__session)
-  ) {
-    console.error(
-      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
-      'Make sure you authorize your request by providing the following HTTP header:',
-      'Authorization: Bearer <Firebase ID Token>',
-      'or by passing a "__session" cookie.'
-    )
-    res.status(403).send('Unauthorized')
-    return
-  }
-
-  let idToken
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer ')
-  ) {
-    console.log('Found "Authorization" header')
-    // Read the ID Token from the Authorization header.
-    idToken = req.headers.authorization.split('Bearer ')[1]
-  } else if (req.cookies) {
-    console.log('Found "__session" cookie')
-    // Read the ID Token from cookie.
-    idToken = req.cookies.__session
-  } else {
-    // No cookie
-    res.status(403).send('Unauthorized')
-    return
-  }
-
-  try {
-    const decodedIdToken = await admin.auth().verifyIdToken(idToken)
-    console.log('ID Token correctly decoded', decodedIdToken)
-    req.user = decodedIdToken
-    next()
-    return
-  } catch (error) {
-    console.error('Error while verifying Firebase ID token:', error)
-    res.status(403).send('Unauthorized')
-    return
-  }
-}
+const validateFirebaseIdToken = require('./auth')
 
 app.use(cors)
 app.use(cookieParser)
 app.use(validateFirebaseIdToken)
+
 app.get('/hello', (req, res) => {
   res.send(`Hello ${req.user.name}`)
+})
+
+app.post('/tasks', (req, res) => {
+  firebaseHelper.firestore.createNewDocument(db, tasksCollection, req.body)
+  res.send('Create a new task')
+})
+// Update new task
+app.patch('/tasks/:taskId', (req, res) => {
+  firebaseHelper.firestore.updateDocument(
+    db,
+    tasksCollection,
+    req.params.taskId,
+    req.body
+  )
+  res.send('Update a new task')
+})
+// View a task
+app.get('/tasks/:taskId', (req, res) => {
+  firebaseHelper.firestore
+    .getDocument(db, tasksCollection, req.params.taskId)
+    .then(doc => res.status(200).send(doc))
+})
+// View all tasks
+app.get('/tasks', (req, res) => {
+  firebaseHelper.firestore
+    .backup(db, tasksCollection)
+    .then(data => res.status(200).send(data))
+})
+// Delete a task
+app.delete('/tasks/:taskId', (req, res) => {
+  firebaseHelper.firestore.deleteDocument(
+    db,
+    tasksCollection,
+    req.params.taskId
+  )
+  res.send('Document deleted')
 })
 
 // This HTTPS endpoint can only be accessed by your Firebase Users.
