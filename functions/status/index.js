af80114442e90fef222807f72766548beba96cf6
Filipe Merker <filipesmerker@gmail.com>
2018-12-09 20:17:57 -0200

    Add more acurate status codes

--- functions/status/index.js
+++ functions/status/index.js
@@ -3,6 +3,14 @@ module.exports = {
     status: 400,
     message: 'Something went wrong'
   },
+  unauthorized: {
+    status: 401,
+    message: 'You must log in'
+  },
+  forbidden: {
+    status: 403,
+    message: "ou don't have permisson to do that"
+  },
   success: {
     status: 200,
     message: 'Success'
