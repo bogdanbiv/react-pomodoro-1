a11f2b0cefde2d542140979aec276d90772c4d06
Filipe Merker <filipesmerker@gmail.com>
2019-01-08 19:45:18 -0200

    Add production build of firebase

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
