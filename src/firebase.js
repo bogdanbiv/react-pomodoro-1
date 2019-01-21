7ec32ababb22072b045a6547f54470fb1e4245a1
Bogdan Bivolaru <bogdan.bivolaru@gmail.com>
2019-01-22 00:59:32 +0200

    remove secret

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAqVUeCGy-ya5ck6JwNePsw9lbQwyRMcDM',
+  apiKey: '',
   authDomain: 'react-pomodoro-2018.firebaseapp.com',
   databaseURL: 'https://react-pomodoro-2018.firebaseio.com',
   projectId: 'react-pomodoro-2018',
